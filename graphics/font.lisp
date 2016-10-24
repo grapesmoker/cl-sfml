ff95c8f618c4f3825e27cb6e8b46003aad5b5775
Jerry Vinokurov <grapesmoker@gmail.com>
2016-10-23 21:45:27 -0400

    font

--- graphics/font.lisp
+++ graphics/font.lisp
@@ -29,7 +29,7 @@
 
 (defcfun ("sfFont_createFromMemory" sf-font-create-from-memory) :pointer
   (data :pointer)
-  (size-in-bytes :unsigned-int))
+  (size-in-bytes :unsigned-int))o
 
 (defcfun ("sfFont_copy" sf-font-copy) :pointer
   (font :pointer))
